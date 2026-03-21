a961d3d0bb0bdf423c0ebad8a0b57b0f8889b0a6
queezz <queezz@gmail.com>
2026-03-21 23:12:28 +0900

    dev branch: switching site template

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1764520944',resources: [
+    cacheName: 'chirpy-1764521067',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
